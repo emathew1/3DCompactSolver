fb35a89a2fc70ab018ca2572d1c2a29fec6c8b7b
emathew1 <emathew1@nd.edu>
2018-02-08 10:43:22 -0600

    Have working visit visualization output in postprocessing

--- PostProcess.cpp
+++ PostProcess.cpp
@@ -69,9 +69,14 @@ int main(int argc, char *argv[]){
                     bcZType, bcZ0, bcZ1);
 
     
+    //create the ideal gas object...
+    double mu_ref = 0.0035;
+    IdealGas *ig = new IdealGas(dom, mu_ref);
+
     //Initialize the derivative objects we need...
     Derivatives *derivX, *derivY, *derivZ;
 
+    cout << "  > Initializing the derivative objects " << endl;
     derivX = new Derivatives(dom, bc->bcXType, Derivatives::DIRX);
     derivY = new Derivatives(dom, bc->bcYType, Derivatives::DIRY);
     derivZ = new Derivatives(dom, bc->bcZType, Derivatives::DIRZ);
@@ -80,6 +85,7 @@ int main(int argc, char *argv[]){
     double *rho, *rhoU, *rhoV, *rhoW, *rhoE;
     double *U, *V, *W, *T, *p;
 
+    cout << "  > Allocating base memory " << endl;
     rho  = new double[Nx*Ny*Nz];
     rhoU = new double[Nx*Ny*Nz];
     rhoV = new double[Nx*Ny*Nz];
@@ -96,9 +102,9 @@ int main(int argc, char *argv[]){
     double temp;
 
     //loading density file
-    cout << "Loading density file...";
+    cout << "  > Loading density file...";
     ifstream infile;
-    string densityFile = "rho.out.5000";
+    string densityFile = "ShearLayer/A3_FullDomain_k6nocurl_10oversqrt3percpert/rho.out.5000";
     infile.open(densityFile);
     if(!infile){
 	cout << "Unable to open density file: " << densityFile << endl;
@@ -111,10 +117,11 @@ int main(int argc, char *argv[]){
     }
     cout << "done!" << endl;
     infile.close();
+    getRange(rho, "RHO", Nx, Ny, Nz);
 
     //loading x momentum file
-    cout << "Loading rhoU file...";
-    string rhoUFile = "rhoU.out.5000";
+    cout << "  > Loading rhoU file...";
+    string rhoUFile = "ShearLayer/A3_FullDomain_k6nocurl_10oversqrt3percpert/rhoU.out.5000";
     infile.open(rhoUFile);
     if(!infile){
 	cout << "Unable to open rhoU file: " << rhoUFile << endl;
@@ -127,10 +134,11 @@ int main(int argc, char *argv[]){
     }
     cout << "done!" << endl;
     infile.close();
+    getRange(rhoU, "RHOU", Nx, Ny, Nz);
 
     //loading y momentum file
-    cout << "Loading rhoV file...";
-    string rhoVFile = "rhoV.out.5000";
+    cout << "  > Loading rhoV file...";
+    string rhoVFile = "ShearLayer/A3_FullDomain_k6nocurl_10oversqrt3percpert/rhoV.out.5000";
     infile.open(rhoVFile);
     if(!infile){
 	cout << "Unable to open rhoV file: " << rhoVFile << endl;
@@ -144,9 +152,11 @@ int main(int argc, char *argv[]){
     cout << "done!" << endl;
     infile.close();
 
+    getRange(rhoV, "RHOV", Nx, Ny, Nz);
+
     //loading z momentum file
-    cout << "Loading rhoW file...";
-    string rhoWFile = "rhoW.out.5000";
+    cout << "  > Loading rhoW file...";
+    string rhoWFile = "ShearLayer/A3_FullDomain_k6nocurl_10oversqrt3percpert/rhoW.out.5000";
     infile.open(rhoWFile);
     if(!infile){
 	cout << "Unable to open rhoW file: " << rhoWFile << endl;
@@ -161,8 +171,8 @@ int main(int argc, char *argv[]){
     infile.close();
 
     //Loading energy file
-    cout << "Loading rhoE file...";
-    string rhoEFile = "rhoE.out.5000";
+    cout << "  > Loading rhoE file...";
+    string rhoEFile = "ShearLayer/A3_FullDomain_k6nocurl_10oversqrt3percpert/rhoE.out.5000";
     infile.open(rhoEFile);
     if(!infile){
 	cout << "Unable to open rhoE file: " << rhoEFile << endl;
@@ -175,8 +185,110 @@ int main(int argc, char *argv[]){
     }
     cout << "done!" << endl;
     infile.close();
+    getRange(rhoE, "RHOE", Nx, Ny, Nz);
 
 
+    cout << "  > Calculating ideal gas data from the input files." << endl;
+
+    #pragma omp parallel for
+    FOR_XYZ{
+	U[ip] = rhoU[ip]/rho[ip];
+	V[ip] = rhoV[ip]/rho[ip];
+	W[ip] = rhoW[ip]/rho[ip];
+	p[ip] = ig->solvep(rho[ip], rhoE[ip], U[ip], V[ip], W[ip]); 
+	T[ip] = ig->solveT(rho[ip], p[ip]);		
+    }
+    cout << "  > Done!" << endl;
+
+    //Calculating cross derivatives of velocity to get vorticity...
+    double *Uy, *Uz;
+    double *Vx, *Vz;
+    double *Wx, *Wy;
+    double *transTemp1, *transTemp2;
+
+    cout << "  > Allocating memory to calculate vorticity..." << endl;
+    Uy = new double[Nx*Ny*Nz];
+    Uz = new double[Nx*Ny*Nz];
+    Vx = new double[Nx*Ny*Nz];
+    Vz = new double[Nx*Ny*Nz];
+    Wx = new double[Nx*Ny*Nz];
+    Wy = new double[Nx*Ny*Nz];
+    transTemp1 = new double[Nx*Ny*Nz];
+    transTemp2 = new double[Nx*Ny*Nz];
+    cout << "  > Done!" << endl;
+
+    int blocksize = 16;
+    
+    //X-Derivatives...
+    cout << "  > X-Derivatives..." << endl;
+    derivX->calc1stDerivField(V, Vx);
+    derivX->calc1stDerivField(W, Wx);
+
+    
+    cout << "  > Y-Derivatives..." << endl;
+    transposeXYZtoYZX_Fast(U, Nx, Ny, Nz, transTemp1, blocksize);
+    derivY->calc1stDerivField(transTemp1, transTemp2);
+    transposeYZXtoXYZ_Fast(transTemp2, Nx, Ny, Nz, Uy, blocksize);
+
+    transposeXYZtoYZX_Fast(W, Nx, Ny, Nz, transTemp1, blocksize);
+    derivY->calc1stDerivField(transTemp1, transTemp2);
+    transposeYZXtoXYZ_Fast(transTemp2, Nx, Ny, Nz, Wy, blocksize);
+
+    cout << "  > Z-Derivatives..." << endl;
+    transposeXYZtoZXY_Fast(U, Nx, Ny, Nz, transTemp1, blocksize);
+    derivZ->calc1stDerivField(transTemp1, transTemp2);
+    transposeZXYtoXYZ_Fast(transTemp2, Nx, Ny, Nz, Uz, blocksize);
+
+    transposeXYZtoZXY_Fast(V, Nx, Ny, Nz, transTemp1, blocksize);
+    derivZ->calc1stDerivField(transTemp1, transTemp2);
+    transposeZXYtoXYZ_Fast(transTemp2, Nx, Ny, Nz, Vz, blocksize);
+    cout << " > Done! " << endl;
+
+    delete[] transTemp1;
+    delete[] transTemp2;
+
+    double *vortMag = new double[Nx*Ny*Nz];
+    
+    #pragma omp parallel for
+    FOR_XYZ{
+	double vortx = Wy[ip] - Vz[ip];
+	double vorty = Uz[ip] - Wx[ip];
+	double vortz = Vx[ip] - Uy[ip];
+	vortMag[ip] = sqrt(vortx*vortx + vorty*vorty + vortz*vortz);
+    }
+
+    getRange(vortMag, "VORTMAG", Nx, Ny, Nz);
+
+
+    //output needs to be in float format...
+    float *rhof = new float[Nx*Ny*Nz];
+    float *pf = new float[Nx*Ny*Nz];
+    float *Tf = new float[Nx*Ny*Nz];
+    float *Uf = new float[Nx*Ny*Nz];
+    float *Vf = new float[Nx*Ny*Nz];
+    float *Wf = new float[Nx*Ny*Nz];
+    float *vortMagf = new float[Nx*Ny*Nz];
+  
+    #pragma omp parallel for
+    FOR_XYZ{
+	rhof[ip] = (float)rho[ip];
+	pf[ip]   = (float)p[ip];
+	Tf[ip]   = (float)T[ip];
+	Uf[ip]   = (float)U[ip];
+	Vf[ip]   = (float)V[ip];
+	Wf[ip] 	 = (float)W[ip];
+	vortMagf[ip] = (float)vortMag[ip];
+
+    }
+   
+
+    const int numvars = 7;
+    int dims[] = {Nx, Ny, Nz};
+    int vardims[] = {1, 1, 1, 1, 1, 1, 1};
+    int centering[] = {1, 1, 1, 1, 1, 1, 1};
+    const char * const varnames[] = {"RHO", "P", "U", "V", "W", "T", "VORTMAG"};
+    float *arrays[] = {(float*)rhof, (float*)pf, (float*)Uf, (float*)Vf, (float*)Wf, (float*)Tf, (float*)vortMagf};
+    write_regular_mesh("test.vtk", !0, dims, numvars, vardims,  centering, varnames, arrays);
 
     return 0;
 }
